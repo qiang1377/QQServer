aab6a22aaa2aa850c0935be918efccd43704f006
zq <zq@qq.com>
2022-02-23 20:56:19 +0800

    first commit

---
+++ QQServer/src/MessageType.java
@@ -0,0 +1,9 @@
+/**
+ * @author 张强
+ * @version 1.0
+ */
+public interface MessageType {
+   // 在接口中定义一些常量的值，不同的常量的值，表示不同的消息类型
+   String MESSAGE_LOGIN_SUCCESS = "1";// 表示登录成功
+   String MESSAGE_LOGIN_FAIL = "0"; // 表示登录失败
+}
